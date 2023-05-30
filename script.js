38069bdc0d7780c7375f8c397e88b5f45a9a893f
hoffmanick <134728053+hoffmanick@users.noreply.github.com>
2023-05-30 08:35:55 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("demo").innerHTML = window.location.href;
+
+const btns = document.getElementsByClassName("btn");
+
+document.getElementById("nick").innerHTML = btns[0].href;
+
+//const hm = location.pathname;
